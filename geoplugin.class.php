a99b206a1bdd2783c00c19e7ee2d0f051ff357aa
Bhathiya Kariyawasam <37865893+BhathiyaTK@users.noreply.github.com>
2019-12-01 18:21:44 +0530

    Update geoplugin.class.php

--- geoplugin.class.php
+++ geoplugin.class.php
@@ -1,30 +1,4 @@
 <?php
-/*
-This PHP class is free software: you can redistribute it and/or modify
-the code under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version. 
-
-However, the license header, copyright and author credits 
-must not be modified in any form and always be displayed.
-
-This class is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-@author geoPlugin (gp_support@geoplugin.com)
-@copyright Copyright geoPlugin (gp_support@geoplugin.com)
-$version 1.2
-
-
-This PHP class uses the PHP Webservice of http://www.geoplugin.com/ to geolocate IP addresses
-
-Geographical location of the IP address (visitor) and locate currency (symbol, code and exchange rate) are returned.
-
-See http://www.geoplugin.com/webservices/php for more specific details of this free service
-
-*/
 
 class geoPlugin {
 	
@@ -36,17 +10,6 @@ class geoPlugin {
 	
 	//the default language
 	var $lang = 'en';
-/*
-supported languages:
-de
-en
-es
-fr
-ja
-pt-BR
-ru
-zh-CN
-*/
 
 	//initiate the geoPlugin vars
 	var $ip = null;
